44a67903c2e730ebb759c27e3f344e7a9dbf66df
KasparStepan <xkaspa43@vutbr.cz>
2024-05-06 20:00:07 +0200

    Fully working wing, fuselage needs to be added and panel method working

--- Drone_OpenVSP.py
+++ Drone_OpenVSP.py
@@ -1,12 +1,39 @@
 import openvsp as vsp
 import numpy as np
+import os
 
 vsp.VSPRenew()
 vsp.ClearVSPModel()
 
 ## Main wing 
 main_wing = vsp.AddGeom("WING")
-vsp.InsertXSec(main_wing,1,vsp.XS_FOUR_SERIES)
+xsec_main_wing = vsp.GetXSecSurf(main_wing, 0 )
+
+## Split wing into two sections
+vsp.InsertXSec(main_wing,1,vsp.XS_FILE_AIRFOIL)
+
+
+## Set crossection to use dat file for airfoil import
+
+vsp.ChangeXSecShape(xsec_main_wing,0,vsp.XS_FILE_AIRFOIL)
+xsec_1 = vsp.GetXSec(xsec_main_wing, 0)
+vsp.ReadFileAirfoil(xsec_1, 'airfoil_database/rg15.dat')
+vsp.Update()
+
+
+
+vsp.ChangeXSecShape(xsec_main_wing,1,vsp.XS_FILE_AIRFOIL)
+xsec_2 = vsp.GetXSec(xsec_main_wing, 1)
+vsp.ReadFileAirfoil(xsec_2, "airfoil_database/rg15.dat")
+vsp.Update()
+
+
+
+vsp.ChangeXSecShape(xsec_main_wing,2,vsp.XS_FILE_AIRFOIL)
+xsec_3 = vsp.GetXSec(xsec_main_wing, 2)
+vsp.ReadFileAirfoil(xsec_3, "airfoil_database/rg15.dat")
+vsp.Update()
+
 
 # Center section
 
@@ -36,12 +63,23 @@ vsp.Update()
 
 ## Tail wing 
 tail_wing = vsp.AddGeom("WING")
-'''
+
 xsec_tail_wing = vsp.GetXSecSurf(tail_wing,0)
-print(xsec_tail_wing)
 vsp.ChangeXSecShape(xsec_tail_wing,0,vsp.XS_FOUR_SERIES)
-print(vsp.GetXSecParmIDs(xsec_tail_wing))
-'''
+vsp.Update()
+
+# Airfoil parameters
+vsp.SetParmVal(tail_wing,"ThickChord","XSecCurve_0",0.12)
+vsp.SetParmVal(tail_wing,"Camber","XSecCurve_0",0)
+
+vsp.Update()
+
+vsp.SetParmVal(tail_wing,"ThickChord","XSecCurve_1",0.12)
+vsp.SetParmVal(tail_wing,"Camber","XSecCurve_1",0)
+
+vsp.Update()
+
+
 section = "XSec_1"
 vsp.SetParmVal(tail_wing,"Span",section, 0.25 )
 vsp.SetParmVal(tail_wing,"Root_Chord",section, 0.1)
@@ -49,6 +87,12 @@ vsp.SetParmVal(tail_wing,"Tip_Chord",section, 0.03)
 vsp.SetParmVal(tail_wing,"Sweep", section, 0)
 vsp.SetParmVal(tail_wing, "Dihedral", section, 30)
 vsp.SetParmVal(tail_wing, "X_Rel_Location", "XForm",0.65)
+vsp.SetParmVal(tail_wing,"Sec_Sweep_Location",section,0.01)
+vsp.SetParmVal(tail_wing,"Sweep_Location", section, 1)
+
+
+
+
 
 vsp.Update()
 
